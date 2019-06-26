72780b642d0024693bf0d87fb5b3951a496ffb1a
Shiblu <faisalalamshiblu@gmail.com>
2019-06-26 20:00:55 +0600

    part 13

--- E-Commerce/OnlineShoppingStore/Controllers/HomeController.cs
+++ E-Commerce/OnlineShoppingStore/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using OnlineShoppingStore.Models.Home;
+﻿using OnlineShoppingStore.DAL;
+using OnlineShoppingStore.Models.Home;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,18 +10,59 @@ namespace OnlineShoppingStore.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly OnlineShoppingEntities _db;
+
+        public HomeController()
+        {
+            _db = new OnlineShoppingEntities();
+        }
+
         public ActionResult Index(string search, int? page)
         {
             HomeIndexViewModel model = new HomeIndexViewModel();
             return View(model.CreateModel(search, 8, page));
         }
 
-        public ActionResult About()
+        public ActionResult AddToCart(int productId)
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
+            if (Session["cart"] == null)
+            {
+                List<Item> cart = new List<Item>();
+                var product = _db.Tbl_Product.Find(productId);
+                cart.Add(new Item()
+                {
+                    Product = product,
+                    Quantity = 1
+                });
+                Session["cart"] = cart;
+            }
+            else
+            {
+                List<Item> cart = (List<Item>)Session["cart"];
+                var product = _db.Tbl_Product.Find(productId);
+                cart.Add(new Item()
+                {
+                    Product = product,
+                    Quantity = 1
+                });
+                Session["cart"] = cart;
+            }
+            return Redirect("Index");
         }
 
+        public ActionResult RemoveFromCart(int productId)
+        {
+            List<Item> cart = (List<Item>)Session["cart"];
+            foreach (var item in cart)
+            {
+                if (item.Product.ProductId == productId)
+                {
+                    cart.Remove(item);
+                    break;
+                }
+            }
+            Session["cart"] = cart;
+            return Redirect("Index");
+        }
     }
 }
